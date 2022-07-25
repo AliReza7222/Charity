bfc32a8c90c7187fb06a0234658fa18896633ec3
seyed alireza <ali.r.salehi99@gmail.com>
2022-07-26 02:05:36 +0430

    change admin.py

--- accounts/admin.py
+++ accounts/admin.py
@@ -15,3 +15,5 @@ class AdminPanel(admin.ModelAdmin):
         'user_permissions'
     ]
     filter_horizontal = ['groups', 'user_permissions']
+    list_display = ['username', 'email', 'first_name', 'last_name', 'is_staff', 'is_active']
+    list_editable = ['is_staff', 'is_active']

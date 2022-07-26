7eb63d3113392788954ae282d9780639ace0d928
seyed alireza <ali.r.salehi99@gmail.com>
2022-07-26 17:52:04 +0430

    create a homepage

--- about_us/urls.py
+++ about_us/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import about_us
+from .views import about_us, homepage
 
 
 name = 'about_us'
 urlpatterns = [
-    path('about', about_us, name='about')
+    path('about', about_us, name='about'),
+    path('home', homepage, name='home'),
 ]
--- about_us/views.py
+++ about_us/views.py
@@ -2,10 +2,20 @@ from django.shortcuts import render
 from accounts.models import User
 
 
-def about_us(request):
+def context_dict():
     users = User.objects.all()
     context = dict()
     context['users'] = users
+    return context
+
+
+def about_us(request):
+    context = context_dict()
 
     if request.method == 'GET':
         return render(request, 'about_us.html', context=context)
+
+
+def homepage(request):
+    context = context_dict()
+    return render(request, 'homepage.html', context=context)

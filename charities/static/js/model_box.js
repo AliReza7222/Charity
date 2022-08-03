1ec23bdecfea7fa9117e830f9b976f2eb69a6073
seyed alireza <ali.r.salehi99@gmail.com>
2022-08-03 19:03:36 +0430

    delete a file js

--- charities/static/js/model_box.js
+++
@@ -1,7 +0,0 @@
-$( document ).ready(function() {
-  $('.trigger').on('click', function() {
-     $('.modal-wrapper').toggleClass('open');
-    $('.page-wrapper').toggleClass('blur-it');
-     return false;
-  });
-});

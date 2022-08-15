39ff6b54430aecefb9b8fb5618e7efc0e3aba1a6
seyed alireza <ali.r.salehi99@gmail.com>
2022-08-15 13:18:06 +0430

    comment app about_us

--- about_us/urls.py
+++ about_us/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from .views import about_us, homepage
 
-
+# Create url path for views app about_us
 name = 'about_us'
 urlpatterns = [
     path('about/', about_us, name='about'),
--- about_us/views.py
+++ about_us/views.py
@@ -1,24 +1,27 @@
 from django.shortcuts import render
 from accounts.models import User
-from django.contrib.auth import logout
 
 
+# Create a function create context for use in views about_us and homepage
 def context_dict(request):
     users = User.objects.all()
     context = dict()
     context['users'] = users
     context['user_site'] = None
+    # if user login in site => context['user_site'] = info_user
     if request.user.is_authenticated:
         context['user_site'] = request.user
 
     return context
 
 
+# Create fbv view page about_us
 def about_us(request):
     context = context_dict(request)
     return render(request, 'about_us.html', context=context)
 
 
+# Create fbv view page home
 def homepage(request):
     context = context_dict(request)
     return render(request, 'homepage.html', context=context)
